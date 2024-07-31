5df256f9dcccdf59d70d377e3759962268bc6fb3
kuroro1208 <yamagata_7580@yahoo.co.jp>
2024-07-31 23:27:28 +0900

    コミュニティ作成機能実装

--- app/Http/Controllers/Backend/CommunityController.php
+++ app/Http/Controllers/Backend/CommunityController.php
@@ -3,16 +3,21 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CommunityController\StoreRequest;
+use App\Models\Community;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class CommunityController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): View
     {
-        //
+        return view('OK');
     }
 
     /**
@@ -20,15 +25,17 @@ class CommunityController extends Controller
      */
     public function create()
     {
-        //
+        return Inertia::render('Communities/Create');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreRequest $request): RedirectResponse
     {
-        //
+        Community::create($request->validated() + ['user_id' => auth()->id()]);
+
+        return to_route('communities.index');
     }
 
     /**
--- app/Http/Requests/CommunityController/StoreRequest.php
+++ app/Http/Requests/CommunityController/StoreRequest.php
@@ -11,7 +11,7 @@ class StoreRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,18 @@ class StoreRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => ['required', 'unique:communities'],
+            'slug' => ['required', 'unique:communities'],
+            'description' => ['required', 'min:5', 'max:255']
+        ];
+    }
+
+    public function attributes(): array
+    {
+        return [
+            'name' => 'コミュニティ名',
+            'slug' => '別名',
+            'description' => 'コミュニティの概要'
         ];
     }
 }
